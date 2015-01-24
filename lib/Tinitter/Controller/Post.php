d9e6eb9510e136a618c57ac07efe01ab7ad9f3dc
UCHIDA Masaru <usadamasa@gmail.com>
2015-01-24 13:24:40 +0900

    投稿フォームのバリデーションを実装

--- lib/Tinitter/Controller/Post.php
+++ lib/Tinitter/Controller/Post.php
@@ -1,6 +1,7 @@
 <?php
 namespace Tinitter\Controller;
 use \Tinitter\Model\Post as M_Post;
+use \Tinitter\Validator\Post as V_Post;
 
 class Post
 {
@@ -8,7 +9,20 @@ class Post
     {
        $app = \Slim\Slim::getInstance();
        $params = $app->request->params();
+       $error_list = V_Post::byArray($params);
 
+       if(!empty($error_list))
+       {
+           $app->render(
+                'TimeLine/show.twig',
+                [
+                    'params' => $params,
+                    'error_list' => $error_list
+                ]
+           );
+
+           return;
+       }
        $post = new M_Post;
        $post->nickname  = $params['nickname'];
        $post->body      = $params['body'];
